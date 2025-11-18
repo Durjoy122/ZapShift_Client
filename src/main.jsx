1a902a74efcaedce73c322774c6a5a94a0b3cc77
Durjoy122 <durjoysarker520@gmail.com>
2025-11-18 11:51:42 +0600

    add footer

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import './index.css'
 import router from './routes/router.jsx';
 import App from './App.jsx'
 import { RouterProvider } from "react-router/dom";
+import AuthProvider from './contexts/AuthProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />,
+    <AuthProvider>
+       <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,7 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "../layouts/RootLayout";
+import AuthLayout from "../layouts/AuthLayout";
 import Home from "../Pages/Home/Home";
-import Coverage from "../Pages/Coverage/Coverage"
+import Coverage from "../Pages/Coverage/Coverage";
+import Login from "../Pages/Auth/Login";
+import Register from "../Pages/Auth/Register";
 
 const router = createBrowserRouter([
     {
@@ -19,6 +22,20 @@ const router = createBrowserRouter([
             }
         ]
     },
+    {
+        path: '/',
+        Component: AuthLayout,
+        children: [
+            {
+                path: 'login',
+                Component: Login
+            },
+            {
+                path: 'register',
+                Component: Register
+            }
+        ]
+    }
 ]);
 
 export default router;
